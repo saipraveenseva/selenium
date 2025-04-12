472df69157139dda63b285d9aaff1212b57c941b
saipraveenseva <saipraveenseva@gmail.com>
2025-04-13 00:41:35 +0530

    Created Selenium project and pushing to git.

---
+++ src/test/java/SeleniumIntroduction.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumIntroduction{
+
+    public static void main(String[] args) {
+
+        //Invoking browser
+        //Chrome - Chrome WebDriver ->Methods close get
+        //Firefox - FirefoxDriver->Methods close get
+
+        WebDriver chromeDriver = new ChromeDriver();
+        WebDriver firefoxDriver = new FirefoxDriver();
+
+
+    }
+}
