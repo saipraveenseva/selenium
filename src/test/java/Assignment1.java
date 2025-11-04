a7a08787a82cce9a4bd3cfcb81727f15c5648958
saipraveenseva <saipraveenseva@gmail.com>
2025-11-04 22:30:06 +0530

    13/10(2)

---
+++ src/test/java/Assignment1.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class Assignment1 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        chromeDriver.findElement(By.id("checkBoxOption1")).click();
+        Assert.assertTrue(chromeDriver.findElement(By.id("checkBoxOption1")).isSelected());
+
+        Thread.sleep(3000);
+
+        chromeDriver.findElement(By.id("checkBoxOption1")).click();
+        Assert.assertFalse(chromeDriver.findElement(By.id("checkBoxOption1")).isSelected());
+
+
+        // <input id="checkBoxOption1" value="option1" name="checkBoxOption1" type="checkbox">
+
+        System.out.println(chromeDriver.findElements(By.cssSelector("input[type='checkbox']")).size());
+    }
+}
